4c4b9917b30ab0d184fc0a9177b3280e1c22099c
Altjin Purevdorj <altjin.p@gmobile.local>
2025-08-29 11:11:58 +0800

    nislegiin tuluw harah bolson bh hha

--- Air.Server/Program.cs
+++ Air.Server/Program.cs
@@ -1,36 +1,64 @@
 ﻿using Air.Server.Data;
+using Air.Server.Hubs;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// DbContext → SQLite
+// DB (SQLite)
 builder.Services.AddDbContext<AppDb>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 
-// Swagger
-builder.Services.AddEndpointsApiExplorer();
+// Controllers + Swagger
+builder.Services.AddControllers()
+    .AddJsonOptions(o =>
+        o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())); builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers();
+// CORS – зөвхөн Display руу зөвшөөрнө
+const string CorsPolicy = "AllowDisplay";
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(CorsPolicy, p => p
+        .WithOrigins("http://localhost:5031")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
+});
+
+// SignalR
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Миграцийг автоматаар ажиллуулна (seed ХИЙХГҮЙ)
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDb>();
-    db.Database.Migrate();
-}
-
+// ---------- Pipeline ORDER ЧУХАЛ ----------
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// HTTPS порт тодорхойгүй анхааруулга байвал алгасъя
-// app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-app.MapControllers();
+// 1) Routing эхлүүлнэ
+app.UseRouting();
+
+// 2) CORS яг энд!
+app.UseCors(CorsPolicy);
+
+// (хэрвээ ашиглавал) auth/authorization
+// app.UseAuthentication();
+app.UseAuthorization();
+
+// 3) Endpoint-уудаа map хийхдээ CORS-оо шаард
+app.MapControllers().RequireCors(CorsPolicy);
+app.MapHub<FlightsHub>("/hubs/flights").RequireCors(CorsPolicy);
+
+// Миграци (seed хийхгүй)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDb>();
+    db.Database.Migrate();
+}
 
 app.Run();
